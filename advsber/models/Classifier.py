1ad74a77282d9914eae789398713c86d9cbce480
Matvey <matveymor@yandex.ru>
2020-09-13 20:09:32 +0300

    Add classifier model and config

---
+++ advsber/models/Classifier.py
@@ -0,0 +1,51 @@
+from typing import Dict, Optional
+
+import torch
+
+from allennlp.data import TextFieldTensors, Vocabulary
+from allennlp.models.model import Model
+from allennlp.modules import Seq2SeqEncoder, TextFieldEmbedder
+from allennlp.training.metrics import CategoricalAccuracy
+
+
+@Model.register("BacicClassifier")
+class BacicClassifier(Model):
+    def __init__(self,
+                vocab: Vocabulary,
+                transactions_field_embedder: TextFieldEmbedder,
+                seq2seq_encoder: Seq2SeqEncoder,
+                amounts_field_embedder: Optional[TextFieldEmbedder] = None,
+                ) -> None:
+
+        super().__init__(vocab)
+        self._transactions_field_embedder = transactions_field_embedder
+        self._amounts_field_embedder = amounts_field_embedder
+        self._seq2seq_encoder = seq2seq_encoder
+        num_labels = vocab.get_vocab_size("labels")
+        self.fc = torch.nn.Linear(128, num_labels)
+        self._loss = torch.nn.CrossEntropyLoss()
+        self._accuracy = CategoricalAccuracy()
+
+    def forward(self,
+                transactions: TextFieldTensors,
+                label: torch.Tensor,
+                amounts: Optional[TextFieldTensors] = None,
+                **kwargs,
+    ) -> Dict[str, torch.Tensor]:
+
+        transaction_embeddings = self._transactions_field_embedder(transactions)
+        if amounts is not None and self._amounts_field_embedder is not None:
+            amount_embeddings = self._amounts_field_embedder(amounts)
+            transaction_embeddings = torch.cat((transaction_embeddings, amount_embeddings), dim=-1)
+
+        contextual_embeddings = self._seq2seq_encoder(transaction_embeddings, mask=None)
+
+        logits = self.fc(contextual_embeddings)
+        probs = torch.nn.functional.softmax(logits)
+        loss = torch.nn.functional.cross_entropy(logits, label)
+        self._accuracy(logits, label)
+        output = {'loss': loss, 'probs': probs}
+        return output
+
+    def get_metrics(self, reset: bool = False) -> Dict[str, float]:
+        return {"accuracy": self.accuracy.get_metric(reset)}
---
+++ configs/language_models/GRU.jsonnet
@@ -0,0 +1,70 @@
+local TOKEN_INDEXER = {
+    "tokens": {
+        "type": "single_id",
+        "start_tokens": [
+          "<START>"
+        ],
+        "end_tokens": [
+          "<END>"
+        ],
+        // should be set to the maximum value of `ngram_filter_sizes`
+        "token_min_padding_length": 5
+      }
+};
+
+{
+  "dataset_reader": {
+    "type": "transactions_reader",
+    "discretizer_path": std.extVar("DISCRETIZER_PATH"),
+    "max_sequence_length": 150,
+    "lazy": false
+  },
+  "train_data_path": std.extVar("LM_TRAIN_DATA_PATH"),
+  "validation_data_path": std.extVar("LM_VALID_DATA_PATH"),
+  "model": {
+    "type": "basic_classifier_one_hot_support",
+    "transactions_field_embedder": {
+      "token_embedders": {
+        "tokens": {
+          "type": "embedding",
+          "embedding_dim": 64,
+          "trainable": true,
+          "vocab_namespace": "transactions"
+        }
+      }
+    },
+    "amounts_field_embedder": {
+      "token_embedders": {
+        "tokens": {
+          "type": "embedding",
+          "embedding_dim": 64,
+          "trainable": true,
+          "vocab_namespace": "amounts"
+        }
+      }
+    },
+    "seq2seq_encoder": {
+        "type": "gru",
+        "input_size": 100,
+        "hidden_size": 128,
+        "num_layers": 1,
+        "dropout": 0.1,
+        "bidirectional": true
+    }
+  },
+  "data_loader": {
+    "batch_size": 64
+  },
+//  "distributed": {
+//    "master_port": 29555,
+//    "cuda_devices": [
+//      0,
+//      1
+//    ]
+//  },
+  "trainer": {
+    "num_epochs": 50,
+    "patience": 2,
+    "cuda_device": 0
+  }
+}
