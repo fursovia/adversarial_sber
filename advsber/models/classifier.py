ae290a69043a973c16c45ef7c7ffe9b86e648c5f
Matvey <matveymor@yandex.ru>
2020-09-13 20:56:59 +0300

    Fix

--- advsber/models/classifier.py
+++ advsber/models/classifier.py
@@ -36,7 +36,7 @@ class BasicClassifier(Model):
         print(transaction_embeddings.shape)
         if amounts is not None and self._amounts_field_embedder is not None:
             amount_embeddings = self._amounts_field_embedder(amounts)
-            transaction_embeddings = torch.cat((transaction_embeddings, amount_embeddings), dim=-1)
+            transaction_embeddings = torch.cat((transaction_embeddings, amount_embeddings), dim=1)
         print(transaction_embeddings.shape)
         contextual_embeddings = self._seq2seq_encoder(transaction_embeddings, mask=None)
         print(contextual_embeddings.shape)
