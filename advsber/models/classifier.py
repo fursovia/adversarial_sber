e2575f092c22678f619567062ce5ef0b29084541
Matvey <matveymor@yandex.ru>
2020-09-13 20:27:26 +0300

    Another small changes

--- advsber/models/classifier.py
+++ advsber/models/classifier.py
@@ -8,7 +8,7 @@ from allennlp.modules import Seq2SeqEncoder, TextFieldEmbedder
 from allennlp.training.metrics import CategoricalAccuracy
 
 
-@Model.register("BacicClassifier")
+@Model.register("BasicClassifier")
 class BacicClassifier(Model):
     def __init__(self,
                 vocab: Vocabulary,
