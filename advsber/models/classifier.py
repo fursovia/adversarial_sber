6c88c52748388d92857ca5154ebc7ab99c349a9e
Matvey <matveymor@yandex.ru>
2020-09-13 21:32:30 +0300

    Fix

--- advsber/models/classifier.py
+++ advsber/models/classifier.py
@@ -22,11 +22,6 @@ class BasicClassifier(Model):
         self._seq2seq_encoder = seq2seq_encoder
         num_labels = vocab.get_vocab_size("label")
         self.fc = torch.nn.Linear(256, num_labels)
-
-        #self._head = LinearLanguageModelHead(vocab=vocab,
-        #                                     input_dim=self._seq2seq_encoder.get_output_dim(),
-        #                                     vocab_namespace="transactions")
-
         self._loss = torch.nn.CrossEntropyLoss()
         self.accuracy = CategoricalAccuracy()
 
@@ -37,18 +32,14 @@ class BasicClassifier(Model):
                 **kwargs,
     ) -> Dict[str, torch.Tensor]:
         transaction_embeddings = self._transactions_field_embedder(transactions)
-        print(transaction_embeddings.shape)
+
         if amounts is not None and self._amounts_field_embedder is not None:
             amount_embeddings = self._amounts_field_embedder(amounts)
             transaction_embeddings = torch.cat((transaction_embeddings, amount_embeddings), dim=-1)
-        shapes = transaction_embeddings.shape
-        #transaction_embeddings = transaction_embeddings.reshape(shapes[1], shapes[0], shapes[2])
-        print(transaction_embeddings.shape)
+
         contextual_embeddings = self._seq2seq_encoder(transaction_embeddings, mask=None)
         contextual_embeddings = torch.mean(contextual_embeddings, dim=1)
         logits = self.fc(contextual_embeddings)
-        #logits = self._head(contextual_embeddings)
-        print(logits.shape, 'AAAAAAAAAA')
         probs = torch.nn.functional.softmax(logits)
         loss = torch.nn.functional.cross_entropy(logits, label)
         self.accuracy(logits, label)
