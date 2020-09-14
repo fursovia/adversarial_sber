f245dc9fc3b7796356b46589b2a38b9263b7bd46
Matvey Morozov <34199760+MatveyMor@users.noreply.github.com>
2020-09-15 00:43:29 +0300

    Update evaluate.py

--- advsber/commands/evaluate.py
+++ advsber/commands/evaluate.py
@@ -2,6 +2,7 @@ import json
 
 import typer
 import pandas as pd
+import numpy as np
 
 from advsber.utils.data import load_jsonlines
 from advsber.utils.metrics import normalized_accuracy_drop, misclassification_error, probability_drop
@@ -10,17 +11,20 @@ from advsber.utils.metrics import normalized_accuracy_drop, misclassification_er
 def main(output_path: str, save_to: str = typer.Option(None), visualize: bool = typer.Option(False)):
     output = load_jsonlines(output_path)
     output = pd.DataFrame(output).drop(columns="history")
-
-    nad = normalized_accuracy_drop(
-        wers=output["wer"], y_true=output["attacked_label"], y_adv=output["adversarial_label"],
-    )
+    
+    y_true = [output['data'][i]['label'] for i in range(len(output))]
+    y_adv = [output['adversarial_data'][i]['label'] for i in range(len(output))]
+    nad = normalized_accuracy_drop(wers=output["wer"], y_true=y_true, y_adv=y_adv)
     typer.echo(f"NAD = {nad:.2f}")
 
-    misclf_error = misclassification_error(y_true=output["attacked_label"], y_adv=output["adversarial_label"],)
+    misclf_error = misclassification_error(y_true=y_true, y_adv=y_adv)
     typer.echo(f"Misclassification Error = {misclf_error:.2f}")
 
     prob_drop = probability_drop(true_prob=output["probability"], adv_prob=output["adversarial_probability"])
     typer.echo(f"Probability drop = {prob_drop:.2f}")
+    
+    mean_wer = np.mean(np.array(output["wer"]))
+    typer.echo(f"Mean WER = {mean_wer:.2f}")
 
     if visualize:
         assert save_to is not None
