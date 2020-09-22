13daed4b76d457a706369c91a35c746c01d4b5ea
fursovia <fursov.ia@gmail.com>
2020-09-22 23:16:09 +0300

    added fgsm

--- advsber/__init__.py
+++ advsber/__init__.py
@@ -1,4 +1,4 @@
 from .models import MaskedLanguageModel, TransactionsClassifier
-from .attackers import AttackerOutput, Attacker, SamplingFool, ConcatSamplingFool
+from .attackers import AttackerOutput, Attacker, SamplingFool, ConcatSamplingFool, FGSM
 from advsber.settings import TransactionsData
 from .dataset_readers import TransactionsDatasetReader
