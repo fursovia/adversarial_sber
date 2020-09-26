15cb099f1b0830482ed0dd1e1ec937eae65d4eae
fursovia <fursov.ia@gmail.com>
2020-09-27 02:15:05 +0300

    added concat fgsm

--- advsber/__init__.py
+++ advsber/__init__.py
@@ -1,4 +1,4 @@
 from .models import MaskedLanguageModel, TransactionsClassifier
-from .attackers import AttackerOutput, Attacker, SamplingFool, ConcatSamplingFool, FGSM
+from .attackers import AttackerOutput, Attacker, SamplingFool, ConcatSamplingFool, FGSM, ConcatFGSM
 from advsber.settings import TransactionsData
 from .dataset_readers import TransactionsDatasetReader
