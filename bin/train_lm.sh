fa5e7aaeb633be9d5e80a77d524f3c28c7c5767b
NinaOwl <ninakaplouh@mail.ru>
2020-10-12 12:37:38 +0300

    fix

--- bin/train_lm.sh
+++ bin/train_lm.sh
@@ -5,7 +5,7 @@
 
 CONFIG_NAME=$1
 DATA_DIR=$2
-DISCRETIZER_NAME=${3:-"discretizer_100_quantile"}
+DISCRETIZER_NAME=${3:-"discretizers/discretizer_100_quantile"}
 DATASET_NAME=$(basename ${DATA_DIR})
 DATE=$(date +%H%M%S-%d%m)
 EXP_NAME=${DATE}-${CONFIG_NAME}
