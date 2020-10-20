6f996741096d5a8d24cd692857c695514f8a39d8
fursovia <fursov.ia@gmail.com>
2020-10-20 19:36:23 +0300

    only when training

--- advsber/allennlp_modules/atraining_callback.py
+++ advsber/allennlp_modules/atraining_callback.py
@@ -23,12 +23,13 @@ class AdversarialTrainingCallback(BatchCallback):
         is_master: bool,
     ) -> None:
 
-        attacker = Attacker.from_params(self.attacker_params)
-        for batch in batch_inputs:
-            adv_batch = attacker.attack(batch)
-            batch_outputs = trainer.batch_outputs(adv_batch, for_training=True)
-            loss = batch_outputs.get("loss")
-            _ = batch_outputs.get("reg_loss")
-            loss.backward()
-            trainer.optimizer.step()
-            trainer.optimizer.zero_grad()
+        if is_training:
+            attacker = Attacker.from_params(self.attacker_params)
+            for batch in batch_inputs:
+                adv_batch = attacker.attack(batch)
+                batch_outputs = trainer.batch_outputs(adv_batch, for_training=True)
+                loss = batch_outputs.get("loss")
+                _ = batch_outputs.get("reg_loss")
+                loss.backward()
+                trainer.optimizer.step()
+                trainer.optimizer.zero_grad()
