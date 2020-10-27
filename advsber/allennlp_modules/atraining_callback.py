dac9223380df912d2945d69da76b74ca4872d5a5
Matvey <matveymor@yandex.ru>
2020-10-27 23:46:41 +0300

    Fix adv train

--- advsber/allennlp_modules/atraining_callback.py
+++ advsber/allennlp_modules/atraining_callback.py
@@ -5,8 +5,10 @@ from allennlp.data.dataloader import TensorDict
 from allennlp.data import Batch, DatasetReader
 from allennlp.common import Params
 
-from advsber.attackers.attacker import Attacker
+#from advsber.attackers.attacker import Attacker
+from advsber.attackers.fgsm import FGSM
 from advsber.settings import TransactionsData
+from tqdm import tqdm
 
 
 @BatchCallback.register("adversarial_training")
@@ -14,7 +16,8 @@ class AdversarialTrainingCallback(BatchCallback):
     def __init__(self, attacker_params: Dict[str, Any], reader: DatasetReader):
         super().__init__()
         self.attacker_params = Params(attacker_params)
-        self.reader = DatasetReader.from_params(self.attacker_params["reader"])
+        print(self.attacker_params)
+        reader = DatasetReader.from_params(self.attacker_params["reader"])
         self.reader = reader
 
     def __call__(
@@ -28,27 +31,43 @@ class AdversarialTrainingCallback(BatchCallback):
         is_master: bool,
     ) -> None:
 
+        batch_inputs = batch_inputs[0]
+        labels = batch_inputs['label'].numpy()
+
+
         if is_training:
 
-            attacker = Attacker(classifier=trainer.model, reader=self.reader, device=-1)
-            for batch in batch_inputs:
+            attacker = FGSM(classifier=trainer.model, reader=self.reader, device=0)
 
-                instances = []
-                for element in batch:
-                    data = TransactionsData.from_tensors(inputs=element, vocab=trainer.model.vocab)
-                    adv_data = attacker.attack(data)
+            instances = []
+            for index in tqdm(range(len(labels))):
 
-                    instance = self.reader.text_to_instance(**adv_data)
-                    instances.append(instance)
+                batch = {'transactions': batch_inputs['transactions']['tokens']['tokens'][index].numpy(),
+                         'amounts': batch_inputs['amounts']['tokens']['tokens'][index].numpy(),
+                         'label': int(labels[index]),
+                         'client_id': int(batch_inputs['client_id'].numpy()[index])}
 
-                new_batch = Batch(instances)
-                new_batch.index_instances(vocab=trainer.model.vocab)
+                data = TransactionsData.from_tensors(inputs=batch, vocab=trainer.model.vocab)
 
-                new_batch = new_batch.as_tensor_dict()
+                adv_data = attacker.attack(data)
+                adv_data = adv_data.to_dict()
 
-                batch_outputs = trainer.batch_outputs(new_batch, for_training=True)
-                loss = batch_outputs.get("loss")
-                _ = batch_outputs.get("reg_loss")
-                loss.backward()
-                trainer.optimizer.step()
-                trainer.optimizer.zero_grad()
+                adv_data = {'transactions': adv_data['adversarial_data']['transactions'],
+                            'amounts': adv_data['adversarial_data']['amounts'],
+                            'label': adv_data['data']['label'],
+                            'client_id': adv_data['data']['client_id']}
+
+                instance = self.reader.text_to_instance(**adv_data)
+                instances.append(instance)
+
+            new_batch = Batch(instances)
+            new_batch.index_instances(vocab=trainer.model.vocab)
+
+            new_batch = new_batch.as_tensor_dict()
+
+            batch_outputs = trainer.batch_outputs(new_batch, for_training=True)
+            loss = batch_outputs.get("loss")
+            _ = batch_outputs.get("reg_loss")
+            loss.backward()
+            trainer.optimizer.step()
+            trainer.optimizer.zero_grad()
--- advsber/settings.py
+++ advsber/settings.py
@@ -1,20 +1,35 @@
 from typing import List, Optional, Dict, Union
 
 import torch
+import numpy as np
+
 from allennlp.data import TextFieldTensors, Vocabulary
 
 from dataclasses import dataclass
 from dataclasses_json import dataclass_json
 
-from advsber.utils.data import decode_indexes
+# from advsber.utils.data import decode_indexes
 from allennlp.nn.util import get_token_ids_from_text_field_tensors
 
+from allennlp.data.vocabulary import Vocabulary
+from allennlp.data.dataset_readers import DatasetReader
 
 START_TOKEN = "<START>"
 END_TOKEN = "<END>"
 MASK_TOKEN = "@@MASK@@"
 
 ModelsInput = Dict[str, Union[TextFieldTensors, torch.Tensor]]
+
+
+def decode_indexes(
+        indexes: torch.Tensor, vocab: Vocabulary, namespace="transactions", drop_start_end: bool = True,
+) -> List[str]:
+    out = [vocab.get_token_from_index(idx.item(), namespace=namespace) for idx in indexes]
+
+    if drop_start_end:
+        return out[1:-1]
+
+    return out
 
 
 @dataclass_json
@@ -33,19 +48,34 @@ class TransactionsData:
 
     @classmethod
     def from_tensors(cls, inputs: ModelsInput, vocab: Vocabulary) -> "TransactionsData":
+        transaction_ids = inputs["transactions"]
+        amount_ids = inputs["amounts"]
+        label = int(inputs["label"])
 
-        transaction_ids = get_token_ids_from_text_field_tensors(inputs["transactions"])[0]
+        pad_start = np.where(transaction_ids == 0)[0]
 
-        amount_ids = get_token_ids_from_text_field_tensors(inputs["amounts"])[0]
-
-        label = inputs["label"][0].item()
+        if len(pad_start):
+            pad_start = pad_start[0]
+            transaction_ids = transaction_ids[:pad_start]
+            amount_ids = amount_ids[:pad_start]
 
         # TODO: also drop paddings
-        transactions = decode_indexes(indexes=transaction_ids, vocab=vocab, namespace="transactions",)
+        transactions = decode_indexes(indexes=transaction_ids, vocab=vocab, namespace="transactions", )
+        # print(transactions)
         transactions = list(map(int, transactions))
 
-        amounts = decode_indexes(indexes=amount_ids, vocab=vocab, namespace="amounts",)
+        amounts = decode_indexes(indexes=amount_ids, vocab=vocab, namespace="amounts", )
         # TODO: convert to floats? transform_amounts
         amounts = list(map(int, amounts))
 
         return cls(transactions=transactions, amounts=amounts, label=label)
+
+
+def data_to_tensors(
+        data: TransactionsData, reader: DatasetReader, vocab: Vocabulary, device: Union[torch.device, int] = -1,
+) -> ModelsInput:
+    instances = Batch([reader.text_to_instance(**data.to_dict())])
+
+    instances.index_instances(vocab)
+    inputs = instances.as_tensor_dict()
+    return move_to_device(inputs, device)
