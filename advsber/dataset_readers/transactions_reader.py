4aaba8058bd691dc7d69621de6f245969282dd20
NinaOwl <kaploukhaya.ne@phyastech.edu>
2020-11-18 11:05:21 +0300

    tr_reader for continuos amounts

--- advsber/dataset_readers/transactions_reader.py
+++ advsber/dataset_readers/transactions_reader.py
@@ -2,10 +2,11 @@ from typing import List, Optional
 import jsonlines
 import math
 import logging
-
+import typer
+import numpy as np
 from allennlp.common.file_utils import cached_path
 from allennlp.data.dataset_readers.dataset_reader import DatasetReader
-from allennlp.data.fields import TextField, LabelField
+from allennlp.data.fields import TextField, LabelField, ArrayField, MetadataField
 from allennlp.data.instance import Instance
 from allennlp.data.token_indexers import SingleIdTokenIndexer
 from allennlp.data.tokenizers import WhitespaceTokenizer, Token
@@ -19,10 +20,8 @@ logger = logging.getLogger(__name__)
 
 @DatasetReader.register("transactions_reader")
 class TransactionsDatasetReader(DatasetReader):
-    def __init__(self, discretizer_path: str, max_sequence_length: int = None, lazy: bool = False,) -> None:
+    def __init__(self, discretizer_path: str = None, max_sequence_length: int = None, lazy: bool = False, ) -> None:
         super().__init__(lazy=lazy)
-
-        self.discretizer = load_discretizer(discretizer_path)
         self._max_sequence_length = max_sequence_length or math.inf
         self._tokenizer = WhitespaceTokenizer()
         self._start_token = Token(START_TOKEN)
@@ -32,34 +31,29 @@ class TransactionsDatasetReader(DatasetReader):
         return [self._start_token] + tokens + [self._end_token]
 
     def text_to_instance(
-        self,
-        transactions: List[int],
-        amounts: List[float],
-        label: Optional[int] = None,
-        client_id: Optional[int] = None,
+            self,
+            transactions: List[int],
+            amounts: List[float],
+            label: Optional[int] = None,
+            client_id: Optional[int] = None,
     ) -> Instance:
 
         transactions = " ".join(map(str, transactions))
-        amounts = transform_amounts(amounts, self.discretizer)
-        amounts = " ".join(amounts)
 
         transactions = self._tokenizer.tokenize(transactions)
-        amounts = self._tokenizer.tokenize(amounts)
-
         transactions = self._add_start_end_tokens(transactions)
-        amounts = self._add_start_end_tokens(amounts)
-
+        amounts = [0] + amounts + [-1]
         fields = {
             "transactions": TextField(transactions, {"tokens": SingleIdTokenIndexer("transactions")}),
-            "amounts": TextField(amounts, {"tokens": SingleIdTokenIndexer("amounts")}),
+            "amounts": ArrayField(np.asarray(amounts)),
         }
-
+        typer.secho(f"Field {fields}", fg="blue")
         if label is not None:
             fields["label"] = LabelField(label=str(label), skip_indexing=False)
 
         if client_id is not None:
             fields["client_id"] = LabelField(label=client_id, skip_indexing=True, label_namespace="client_id")
-
+        typer.secho(f"Saving results to {Instance(fields)}", fg="green")
         return Instance(fields)
 
     def _read(self, file_path: str):
@@ -97,3 +91,5 @@ class TransactionsDatasetReader(DatasetReader):
 
 
 TransactionsDatasetReader.register("from_archive", constructor="from_archive")(TransactionsDatasetReader)
+
+
