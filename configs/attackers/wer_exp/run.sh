660ce88bea6a7c8d6be10a1ca0cdca5037bd9b6e
fursovia <fursov.ia@gmail.com>
2020-11-26 02:14:20 +0300

    mkdir

--- configs/attackers/wer_exp/run.sh
+++ configs/attackers/wer_exp/run.sh
@@ -8,9 +8,12 @@ mkdir -p $RESULTS_DIR
 for i in $(seq 1 10); do
     config_path=concat_fgsm_${i}.jsonnet
     filename="${config_path%.*}"
+    res_path=${RESULTS_DIR}/${filename}
+    mkdir -p ${res_path}
+
     CUDA_VISIBLE_DEVICES="1" \
         DATA_PATH="./datasets/age/test.jsonl" \
-        OUTPUT_PATH=${RESULTS_DIR}/${filename}/output.json \
+        OUTPUT_PATH=${res_path}/output.json \
         MASKED_LM_PATH="./presets/age/models/lm/bert_with_amounts.tar.gz" \
         CLF_PATH="./presets/age/models/substitute_clf/gru_with_amounts.tar.gz" \
         PYTHONPATH=. python advsber/commands/attack.py configs/attackers/wer_exp/${config_path} \
@@ -23,6 +26,8 @@ for i in $(seq 1 10); do
 
     config_path=concat_sampling_fool${i}.jsonnet
     filename="${config_path%.*}"
+    res_path=${RESULTS_DIR}/${filename}
+    mkdir -p ${res_path}
     CUDA_VISIBLE_DEVICES="1" \
         DATA_PATH="./datasets/age/test.jsonl" \
         OUTPUT_PATH=${RESULTS_DIR}/${filename}/output.json \
@@ -36,6 +41,8 @@ for i in $(seq 1 10); do
 
     config_path=greedy_concat_sampling_fool${i}.jsonnet
     filename="${config_path%.*}"
+    res_path=${RESULTS_DIR}/${filename}
+    mkdir -p ${res_path}
     CUDA_VISIBLE_DEVICES="1" \
         DATA_PATH="./datasets/age/test.jsonl" \
         OUTPUT_PATH=${RESULTS_DIR}/${filename}/output.json \
