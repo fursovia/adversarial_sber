c6680bd650f5111d1d960217fa1cc12e597a2dda
fursovia <fursov.ia@gmail.com>
2020-11-26 02:12:56 +0300

    evaluate

--- configs/attackers/wer_exp/run.sh
+++ configs/attackers/wer_exp/run.sh
@@ -10,31 +10,41 @@ for i in $(seq 1 10); do
     filename="${config_path%.*}"
     CUDA_VISIBLE_DEVICES="1" \
         DATA_PATH="./datasets/age/test.jsonl" \
-        OUTPUT_PATH=${RESULTS_DIR}/${filename}.json \
+        OUTPUT_PATH=${RESULTS_DIR}/${filename}/output.json \
         MASKED_LM_PATH="./presets/age/models/lm/bert_with_amounts.tar.gz" \
         CLF_PATH="./presets/age/models/substitute_clf/gru_with_amounts.tar.gz" \
         PYTHONPATH=. python advsber/commands/attack.py configs/attackers/wer_exp/${config_path} \
         --samples 500
+
+    PYTHONPATH=. python advsber/commands/evaluate.py ${RESULTS_DIR}/${filename}/output.json \
+        --save-to=${RESULTS_DIR}/${filename}/metrics.json \
+        --target-clf-path="./presets/age/models/target_clf/lstm_with_amounts.tar.gz"
 
 
     config_path=concat_sampling_fool${i}.jsonnet
     filename="${config_path%.*}"
     CUDA_VISIBLE_DEVICES="1" \
         DATA_PATH="./datasets/age/test.jsonl" \
-        OUTPUT_PATH=${RESULTS_DIR}/${filename}.json \
+        OUTPUT_PATH=${RESULTS_DIR}/${filename}/output.json \
         MASKED_LM_PATH="./presets/age/models/lm/bert_with_amounts.tar.gz" \
         CLF_PATH="./presets/age/models/substitute_clf/gru_with_amounts.tar.gz" \
         PYTHONPATH=. python advsber/commands/attack.py configs/attackers/wer_exp/${config_path} \
         --samples 500
-
+    PYTHONPATH=. python advsber/commands/evaluate.py ${RESULTS_DIR}/${filename}/output.json \
+        --save-to=${RESULTS_DIR}/${filename}/metrics.json \
+        --target-clf-path="./presets/age/models/target_clf/lstm_with_amounts.tar.gz"
 
     config_path=greedy_concat_sampling_fool${i}.jsonnet
     filename="${config_path%.*}"
     CUDA_VISIBLE_DEVICES="1" \
         DATA_PATH="./datasets/age/test.jsonl" \
-        OUTPUT_PATH=${RESULTS_DIR}/${filename}.json \
+        OUTPUT_PATH=${RESULTS_DIR}/${filename}/output.json \
         MASKED_LM_PATH="./presets/age/models/lm/bert_with_amounts.tar.gz" \
         CLF_PATH="./presets/age/models/substitute_clf/gru_with_amounts.tar.gz" \
         PYTHONPATH=. python advsber/commands/attack.py configs/attackers/wer_exp/${config_path} \
         --samples 500
+
+    PYTHONPATH=. python advsber/commands/evaluate.py ${RESULTS_DIR}/${filename}/output.json \
+        --save-to=${RESULTS_DIR}/${filename}/metrics.json \
+        --target-clf-path="./presets/age/models/target_clf/lstm_with_amounts.tar.gz"
 done
