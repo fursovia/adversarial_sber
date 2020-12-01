09292e822f68c606da80f324915bc7568c876947
fursovia <fursov.ia@gmail.com>
2020-12-01 14:09:44 +0300

    pass cuda from outside

--- configs/attackers/sf_exp/run.sh
+++ configs/attackers/sf_exp/run.sh
@@ -7,8 +7,7 @@ for i in $(seq 0 99); do
     config_path=sampling_fool_${i}.json
     mkdir -p ${RESULTS_DIR}/${i}
 
-    CUDA_VISIBLE_DEVICES="1" \
-        PYTHONPATH=. python advsber/commands/attack.py configs/attackers/sf_exp/${config_path} \
+    PYTHONPATH=. python advsber/commands/attack.py configs/attackers/sf_exp/${config_path} \
         --samples 500
 
     PYTHONPATH=. python advsber/commands/evaluate.py ${RESULTS_DIR}/${i}/output.json \
