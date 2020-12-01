85d43dceb7c7a4280fae7f86268d274293c83c91
fursovia <fursov.ia@gmail.com>
2020-12-01 13:53:16 +0300

    run script

---
+++ configs/attackers/sf_exp/run.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+
+RESULTS_DIR=./results/sf_exp
+
+for i in $(seq 0 99); do
+    config_path=sampling_fool_${i}.jsonnet
+
+    CUDA_VISIBLE_DEVICES="1" \
+        PYTHONPATH=. python advsber/commands/attack.py configs/attackers/sf_exp/${config_path} \
+        --samples 500
+
+    PYTHONPATH=. python advsber/commands/evaluate.py ${RESULTS_DIR}/${i}/output.json \
+        --save-to=${RESULTS_DIR}/${i}/metrics.json \
+        --target-clf-path="./presets/age/models/target_clf/lstm_with_amounts.tar.gz"
+
+done
