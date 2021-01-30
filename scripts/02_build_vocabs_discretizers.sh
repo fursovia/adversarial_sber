fdc561cc0d05a492999cf97ad9137274d45315ae
Matvey <matveymor@yandex.ru>
2021-01-30 23:28:55 +0300

    Fix bug with script 02

--- scripts/02_build_vocabs_discretizers.sh
+++ scripts/02_build_vocabs_discretizers.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+
+rm -rf ./presets/gender/vocabs/100_quantile
+rm -rf ./presets/age/vocabs/100_quantile
+
+rm -rf ./presets/gender/discretizers/100_quantile
+rm -rf ./presets/gender/discretizers/50_quantile
+rm -rf ./presets/age/discretizers/100_quantile
+rm -rf ./presets/age/discretizers/50_quantile
+
 PYTHONPATH=. python scripts/python_scripts/train_discretizers.py 'age'
 PYTHONPATH=. python scripts/python_scripts/train_discretizers.py 'gender'
 
