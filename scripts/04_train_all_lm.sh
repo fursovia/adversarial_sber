4dfe98d628cc98ffc4c0c8b8cec564c67e32aa92
Matvey <matveymor@yandex.ru>
2021-01-31 00:05:29 +0300

    minor changes

--- scripts/04_train_all_lm.sh
+++ scripts/04_train_all_lm.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for dataset_name in "gender" "age"; do
+for dataset_name in "age" "gender"; do
     bash scripts/local/train_lm.sh "bert_with_amounts" "100_quantile" ${dataset_name}
 done
 
