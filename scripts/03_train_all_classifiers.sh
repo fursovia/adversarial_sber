677950cb1f63b1c7a7780cb1859a43d6bc2690f8
Matvey <matveymor@yandex.ru>
2021-01-31 16:43:51 +0300

    update train clf and lm

--- scripts/03_train_all_classifiers.sh
+++ scripts/03_train_all_classifiers.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
 
 mkdir ../experiments
-
-rm -rf ../experiments/trained_models
 mkdir ../experiments/trained_models/
 
-
-for dataset_name in "gender" "age"; do
+for dataset_name in "age" "gender"; do
     mkdir ../experiments/trained_models/${dataset_name}/
     for clf_type in "target" "substitute"; do
         mkdir ../experiments/trained_models/${dataset_name}/${clf_type}_clf
         for config_name in "gru_with_amounts" "lstm_with_amounts" "cnn_with_amounts"; do
             bash scripts/local/train_clf.sh ${config_name} ${clf_type} "100_quantile" ${dataset_name}
+            rm -rf ./presets/${dataset_name}/models/${clf_type}/${config_name}.tar.gz
+            cp -r ../experiments/trained_models/${dataset_name}/${clf_type}/${config_name}/model.tar.gz ./presets/${dataset_name}/models/${clf_type}/${config_name}.tar.gz
         done
     done
 done
--- scripts/04_train_all_lm.sh
+++ scripts/04_train_all_lm.sh
@@ -2,5 +2,7 @@
 
 for dataset_name in "age" "gender"; do
     bash scripts/local/train_lm.sh "bert_with_amounts" "100_quantile" ${dataset_name}
+    rm -rf ./presets/${dataset_name}/models/lm/bert_with_amounts.tar.gz
+    cp -r ../experiments/trained_models/${dataset_name}/lm/bert_with_amounts/model.tar.gz ./presets/${dataset_name}/models/lm/bert_with_amounts.tar.gz
 done
 
