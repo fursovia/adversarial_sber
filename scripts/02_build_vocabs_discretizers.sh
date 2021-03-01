44cc51f79abcf624bf984ac962236588cdbf9cba
Matvey <matveymor@yandex.ru>
2021-03-02 01:58:41 +0300

    Update dataset load and preproccesing + README

--- scripts/02_build_vocabs_discretizers.sh
+++ scripts/02_build_vocabs_discretizers.sh
@@ -3,18 +3,37 @@
 
 rm -rf ./presets/gender/vocabs/100_quantile
 rm -rf ./presets/age/vocabs/100_quantile
+rm -rf ./presets/age_short/vocabs/100_quantile
+rm -rf ./presets/age_tinkoff/vocabs/100_quantile
+rm -rf ./presets/rosbank/vocabs/100_quantile
 
 rm -rf ./presets/gender/discretizers/100_quantile
 rm -rf ./presets/gender/discretizers/50_quantile
+
 rm -rf ./presets/age/discretizers/100_quantile
 rm -rf ./presets/age/discretizers/50_quantile
 
+rm -rf ./presets/age_short/discretizers/100_quantile
+rm -rf ./presets/age_short/discretizers/50_quantile
+
+rm -rf ./presets/age_tinkoff/discretizers/100_quantile
+rm -rf ./presets/age_tinkoff/discretizers/50_quantile
+
+rm -rf ./presets/rosbank/discretizers/100_quantile
+rm -rf ./presets/rosbank/discretizers/50_quantile
+
+
 PYTHONPATH=. python scripts/python_scripts/train_discretizers.py 'age'
 PYTHONPATH=. python scripts/python_scripts/train_discretizers.py 'gender'
+PYTHONPATH=. python scripts/python_scripts/train_discretizers.py 'age_short'
+PYTHONPATH=. python scripts/python_scripts/train_discretizers.py 'age_tinkoff'
+PYTHONPATH=. python scripts/python_scripts/train_discretizers.py 'rosbank'
 
 PYTHONPATH=. python scripts/python_scripts/build_vocabs.py 'age'
 PYTHONPATH=. python scripts/python_scripts/build_vocabs.py 'gender'
-
+PYTHONPATH=. python scripts/python_scripts/build_vocabs.py 'age_short'
+PYTHONPATH=. python scripts/python_scripts/build_vocabs.py 'age_tinkoff'
+PYTHONPATH=. python scripts/python_scripts/build_vocabs.py 'rosbank'
 
 
 
--- scripts/04_train_all_lm.sh
+++ scripts/04_train_all_lm.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for dataset_name in "age" "gender"; do
+for dataset_name in "age" "age_short" "age_tinkoff" "rosbank"; do
     bash scripts/local/train_lm.sh "bert_with_amounts" "100_quantile" ${dataset_name}
     rm -rf ./presets/${dataset_name}/models/lm/bert_with_amounts.tar.gz
     cp -r ../experiments/trained_models/${dataset_name}/lm/bert_with_amounts/model.tar.gz ./presets/${dataset_name}/models/lm/bert_with_amounts.tar.gz
